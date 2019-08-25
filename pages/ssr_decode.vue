b92c24b6cae985876f660015ba9a38f35e92e92c
determ1ne <443@itszzz.top>
2019-08-25 12:13:29 +0800

    增加对 v2rayN 导出的多项 ss(r)/v2ray 配置的解析支持

--- pages/ssr_decode.vue
+++ pages/ssr_decode.vue
@@ -13,7 +13,7 @@
         <nya-container v-if="results" title="解析成功">
             <pre>{{ results }}</pre>
         </nya-container>
-        
+
         <nya-container title="说明">
             <ul class="nya-list">
                 <li>所有解析均在本地进行，不会收集任何配置</li>
@@ -37,62 +37,69 @@ export default {
     },
     computed: {
         results() {
-            let decodeConfig = this.configText.replace(/#(\w|%)+/, '');
-            if (decodeConfig) {
-                let json;
-                let isss = Boolean(decodeConfig.match(/^ssr?:\/\//));
-                let text = decodeConfig.replace(/(vmess|ssr?):\/\//, '');
-                let decode = Buffer.from(text, 'base64').toString();
-                if (isss) {
-                    let config = new Object();
-                    let ssrconfig = decode.split(':');
-                    if (decodeConfig.match(/^ssr:\/\//)) {
-                        let formatConfig = formatUrl(ssrconfig[5]);
-                        config['IP'] = ssrconfig[0];
-                        config['端口'] = ssrconfig[1];
-                        config['协议'] = ssrconfig[2];
-                        config['加密'] = ssrconfig[3];
-                        config['混淆'] = ssrconfig[4];
-                        if (Object.keys(formatConfig).length)
-                            config['密码'] = Buffer.from(
-                                ssrconfig[5].split('/')[0],
-                                'base64'
-                            ).toString();
-                        if (formatConfig.group)
-                            config['分组'] = Buffer.from(
-                                formatConfig.group,
-                                'base64'
-                            ).toString();
-                        if (formatConfig.remarks)
-                            config['备注'] = Buffer.from(
-                                formatConfig.remarks,
-                                'base64'
-                            ).toString();
-                        if (formatConfig.protoparam)
-                            config['协议参数'] = Buffer.from(
-                                formatConfig.protoparam,
-                                'base64'
-                            ).toString();
-                        if (formatConfig.obfsparam)
-                            config['混淆参数'] = Buffer.from(
-                                formatConfig.obfsparam,
-                                'base64'
-                            ).toString();
+            let rawConfigs = this.configText.split(' ');
+            let configs = [];
+            let decodedConfigs = [];
+            for (const conf of rawConfigs) {
+                if (conf !== '') configs.push(conf.replace(/#\w.*/, ''));
+            }
+
+            if (configs.length) {
+                for (let rawConf of configs) {
+                    let isSs = Boolean(rawConf.match(/^ssr?:\/\//));
+                    let text = rawConf.replace(/(vmess|ssr?):\/\//, '');
+                    let decode = Buffer.from(text, 'base64').toString();
+                    if (isSs) {
+                        let config = {};
+                        let ssrConfig = decode.split(':');
+                        if (rawConf.match(/^ssr:\/\//)) {
+                            let formatConfig = formatUrl(ssrConfig[5]);
+                            config['IP'] = ssrConfig[0];
+                            config['端口'] = ssrConfig[1];
+                            config['协议'] = ssrConfig[2];
+                            config['加密'] = ssrConfig[3];
+                            config['混淆'] = ssrConfig[4];
+                            if (Object.keys(formatConfig).length)
+                                config['密码'] = Buffer.from(
+                                    ssrConfig[5].split('/')[0],
+                                    'base64'
+                                ).toString();
+                            if (formatConfig.group)
+                                config['分组'] = Buffer.from(
+                                    formatConfig.group,
+                                    'base64'
+                                ).toString();
+                            if (formatConfig.remarks)
+                                config['备注'] = Buffer.from(
+                                    formatConfig.remarks,
+                                    'base64'
+                                ).toString();
+                            if (formatConfig.protoparam)
+                                config['协议参数'] = Buffer.from(
+                                    formatConfig.protoparam,
+                                    'base64'
+                                ).toString();
+                            if (formatConfig.obfsparam)
+                                config['混淆参数'] = Buffer.from(
+                                    formatConfig.obfsparam,
+                                    'base64'
+                                ).toString();
+                        } else {
+                            config['加密'] = ssrConfig[0];
+                            config['密码'] = ssrConfig[1].split('@')[0];
+                            config['IP'] = ssrConfig[1].split('@')[1];
+                            config['端口'] = ssrConfig[2];
+                        }
+                        decodedConfigs.push(config);
                     } else {
-                        config['加密'] = ssrconfig[0];
-                        config['密码'] = ssrconfig[1].split('@')[0];
-                        config['IP'] = ssrconfig[1].split('@')[1];
-                        config['端口'] = ssrconfig[2];
+                        try {
+                            decodedConfigs.push(JSON.parse(decode));
+                        } catch (error) {
+                            decodedConfigs.push('解析失败，请检查配置是否正确');
+                        }
                     }
-                    return config;
-                } else {
-                    try {
-                        json = JSON.parse(decode);
-                    } catch (error) {
-                        return '解析失败，请检查配置是否正确';
-                    }
-                    return json;
                 }
+                return decodedConfigs;
             } else {
                 return false;
             }
