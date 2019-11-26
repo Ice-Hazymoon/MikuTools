a22d02d63ac89b730e3400173492dbcb30d71b0a
any86 <383514580@qq.com>
2019-11-26 10:33:33 +0800

    A:  同步更新"any-rule"正则
    新增:
    1. 必须带端口号的网址(或ip)
    2. 网址(支持端口和"?+参数"和"#+参数)
    3. 统一社会信用代码

--- utils/RULES.js
+++ utils/RULES.js
@@ -1,24 +1,38 @@
 module.exports = [
     {
-        title: '迅雷链接',
-        rule: /^thunder:\/\/[a-zA-Z0-9]+=$/,
-        examples: [
-            'thunder://QUEsICdtYWduZXQ6P3h0PXVybjpidGloOjBCQTE0RTUxRkUwNjU1RjE0Qzc4NjE4RjY4NDY0QjZFNTEyNjcyOUMnWlo='
-        ]
+        title: '必须带端口号的网址(或ip)',
+        rule: /^(((ht|f)tps?):\/\/)?[\w\-]+(\.[\w\-]+)+:\d{0,5}\/?/,
+        examples: ['https://www.qq.com:8080', '127.0.0.1:5050','baidu.com:8001', 'http://192.168.1.1:9090'],
+        counterExamples: ['192.168.1.1', 'https://www.jd.com']
     },
     {
-        title: 'ed2k链接(宽松匹配)',
-        rule: /^ed2k:\/\/|file|.+|\/$/,
-        examples: [
-            'ed2k://|file|%E5%AF%84%E7%94%9F%E8%99%AB.PARASITE.2019.HD-1080p.X264.AAC-UUMp4(ED2000.COM).mp4|2501554832|C0B93E0879C6071CBED732C20CE577A3|h=5HTKZPQFYRKORN52I3M7GQ4QQCIHFIBV|/'
-        ]
+        title: '网址(支持端口和"?+参数"和"#+参数)',
+        rule: /^(((ht|f)tps?):\/\/)?[\w\-]+(\.[\w\-]+)+([\w\-.,@?^=%&:\/~+#]*[\w\-@?^=%&\/~+#])?$/,
+        examples: ['www.qq.com', 'https://baidu.com', '360.com:8080/vue/#/a=1&b=2'],
+        counterExamples: ['....']
     },
+    {
+        title: '统一社会信用代码',
+        rule: /^[0-9A-HJ-NPQRTUWXY]{2}\d{6}[0-9A-HJ-NPQRTUWXY]{10}$/,
+        examples: ['91230184MA1BUFLT44', '92371000MA3MXH0E3W'],
+    },
+
+    {
+        title: '迅雷链接',
+        rule: /^thunderx?:\/\/[a-zA-Z\d]+=$/,
+        examples: ['thunder://QUEsICdtYWduZXQ6P3h0PXVybjpidGloOjBCQTE0RTUxRkUwNjU1RjE0Qzc4NjE4RjY4NDY0QjZFNTEyNjcyOUMnWlo='],
+    },
+
+    {
+        title: 'ed2k链接(宽松匹配)',
+        rule: /^ed2k:\/\/\|file\|.+\|\/$/,
+        examples: ['ed2k://|file|%E5%AF%84%E7%94%9F%E8%99%AB.PARASITE.2019.HD-1080p.X264.AAC-UUMp4(ED2000.COM).mp4|2501554832|C0B93E0879C6071CBED732C20CE577A3|h=5HTKZPQFYRKORN52I3M7GQ4QQCIHFIBV|/'],
+    },
+
     {
         title: '磁力链接(宽松匹配)',
         rule: /^magnet:\?xt=urn:btih:[0-9a-fA-F]{40,}.*$/,
-        examples: [
-            'magnet:?xt=urn:btih:40A89A6F4FB1498A98087109D012A9A851FBE0FC'
-        ]
+        examples: ['magnet:?xt=urn:btih:40A89A6F4FB1498A98087109D012A9A851FBE0FC'],
     },
     {
         title: '子网掩码',
@@ -43,10 +57,7 @@ module.exports = [
     {
         title: 'window下"文件"路径',
         rule: /^[a-zA-Z]:\\(?:\w+\\)*\w+\.\w+$/,
-        examples: [
-            'C:\\Users\\Administrator\\Desktop\\qq.link',
-            'e:\\m\\vscode.exe'
-        ]
+        examples: ['C:\\Users\\Administrator\\Desktop\\qq.link', 'e:\\m\\vscode.exe']
     },
     {
         title: 'A股代码',
@@ -54,8 +65,7 @@ module.exports = [
         examples: ['sz000858', 'SZ002136', 'sz300675', 'SH600600', 'sh601155']
     },
     {
-        title:
-            '大于等于0, 小于等于150, 支持小数位出现5, 如145.5, 用于判断考卷分数',
+        title: '大于等于0, 小于等于150, 支持小数位出现5, 如145.5, 用于判断考卷分数',
         rule: /^150$|^(?:\d|[1-9]\d|1[0-4]\d)(?:.5)?$/,
         examples: [150, 100.5]
     },
@@ -67,7 +77,7 @@ module.exports = [
     {
         title: 'md5格式(32位)',
         rule: /^[a-f0-9]{32}$/,
-        examples: ['21fe181c5bfc16306a6828c1f7b762e8']
+        examples: ['21fe181c5bfc16306a6828c1f7b762e8'],
     },
     {
         title: '版本号格式必须为X.Y.Z',
@@ -111,9 +121,9 @@ module.exports = [
         examples: [0.99, 8.99, 666]
     },
     {
-        title: '银行卡号（16或19位）',
-        rule: /^(?:[1-9]{1})(?:\d{15}|\d{18})$/,
-        examples: [6222026006705354217]
+        title: '银行卡号（10到30位, 覆盖对公/私账户, 参考[微信支付](https://pay.weixin.qq.com/wiki/doc/api/xiaowei.php?chapter=22_1)）',
+        rule: /^[1-9]\d{9,29}$/,
+        examples: [6234567890, 6222026006705354217]
     },
     {
         title: '中文姓名',
@@ -128,7 +138,7 @@ module.exports = [
     {
         title: '新能源车牌号',
         rule: /[京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领 A-Z]{1}[A-HJ-NP-Z]{1}(([0-9]{5}[DF])|([DF][A-HJ-NP-Z0-9][0-9]{4}))$/,
-        examples: ['京AD92035', '甘G23459F']
+        examples: ['京AD92035', '甘G23459F'],
     },
     {
         title: '非新能源车牌号',
@@ -141,11 +151,6 @@ module.exports = [
         examples: ['京A12345D', '京A00599']
     },
     {
-        title: '网址',
-        rule: /^(?:(?:https?|ftp):\/\/)?(?:[\da-z.-]+)\.(?:[a-z.]{2,6})(?:\/\w\.-]*)*\/?/,
-        examples: ['www.qq.com']
-    },
-    {
         title: '中国手机号(严谨), 根据工信部2019年最新公布的手机号段',
         rule: /^(?:(?:\+|00)86)?1(?:(?:3[\d])|(?:4[5-7|9])|(?:5[0-3|5-9])|(?:6[5-7])|(?:7[0-8])|(?:8[\d])|(?:9[1|8|9]))\d{8}$/,
         examples: ['008618311006933', '+8617888829981', '19119255642']
@@ -156,8 +161,7 @@ module.exports = [
         examples: ['008618311006933', '+8617888829981', '19119255642']
     },
     {
-        title:
-            '中国手机号(最宽松), 只要是1开头即可, 如果你的手机号是用来接收短信, 优先建议选择这一条',
+        title: '中国手机号(最宽松), 只要是1开头即可, 如果你的手机号是用来接收短信, 优先建议选择这一条',
         rule: /^(?:(?:\+|00)86)?1\d{10}$/,
         examples: ['008618311006933', '+8617888829981', '19119255642']
     },
@@ -194,13 +198,7 @@ module.exports = [
     {
         title: '护照（包含香港、澳门）',
         rule: /(^[EeKkGgDdSsPpHh]\d{8}$)|(^(([Ee][a-fA-F])|([DdSsPp][Ee])|([Kk][Jj])|([Mm][Aa])|(1[45]))\d{7}$)/,
-        examples: [
-            's28233515',
-            '141234567',
-            '159203084',
-            'MA1234567',
-            'K25345719'
-        ]
+        examples: ['s28233515', '141234567', '159203084', 'MA1234567', 'K25345719']
     },
     {
         title: '帐号是否合法(字母开头，允许5-16字节，允许字母数字下划线组合',
@@ -259,8 +257,7 @@ module.exports = [
         examples: ['ABC', 'KD']
     },
     {
-        title:
-            '密码强度正则，最少6位，包括至少1个大写字母，1个小写字母，1个数字，1个特殊字符',
+        title: '密码强度正则，最少6位，包括至少1个大写字母，1个小写字母，1个数字，1个特殊字符',
         rule: /^.*(?=.{6,})(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*? ]).*$/,
         examples: ['Kd@curry666']
     },
